b6f3cda22992ec8681ad925f977899517032954b
Joey <xx@gmail.com>
2018-03-13 16:22:28 +0800

    ignore private data

--- corn_data/bot.py
+++
@@ -1,9 +0,0 @@
-
-from telegram.ext import Updater,MessageHandler, Filters,CommandHandler,InlineQueryHandler,RegexHandler
-import telegram
-
-#初始数据
-# bot = telegram.Bot(token="508830944:AAGdJMj2B8BSJ7tlV0oisa4ty2_9dMI386k")
-# updates = bot.get_updates()
-token = '508830944:AAGdJMj2B8BSJ7tlV0oisa4ty2_9dMI386k'
-
